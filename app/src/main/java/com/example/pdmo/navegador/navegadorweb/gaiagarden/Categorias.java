1a6b53f95a96ba3d148215889a12ed4a7a18f661
rm86270 <rm86270@estudante.fieb.edu.br>
2024-08-20 12:19:22 -0300

    tccnovo2

--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Categorias.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Categorias.java
@@ -2,7 +2,9 @@ package com.example.pdmo.navegador.navegadorweb.gaiagarden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Categorias extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Categorias extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
+
+        Button botao = findViewById(R.id.categoriaplantas);
+
+        botao.setOnClickListener((view) -> {
+                    Intent intent = new Intent(this, Plantas.class);
+                    startActivity(intent);
+                });
     }
 }
--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Login.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Login.java
@@ -2,7 +2,9 @@ package com.example.pdmo.navegador.navegadorweb.gaiagarden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button  botao = findViewById(R.id.entrar);
+
+        botao.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+        });
     }
 }
