cbae3a3107cac22af58a2f3655b39c51f8f949f7
rm86270 <rm86270@estudante.fieb.edu.br>
2024-08-26 08:27:07 -0300

    tccnovo220

--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Home.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Home.java
@@ -2,13 +2,32 @@ package com.example.pdmo.navegador.navegadorweb.gaiagarden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+
+    ImageView estrela;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        estrela = findViewById(R.id.estrela);
+
+        estrela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), Meus_Favoritos.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
