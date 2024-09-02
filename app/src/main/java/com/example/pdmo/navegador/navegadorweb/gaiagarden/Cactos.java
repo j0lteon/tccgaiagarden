652e9484ef922f3c75754922522519216f728a58
rm86270 <rm86270@estudante.fieb.edu.br>
2024-09-02 15:08:18 -0300

    tccnovo222

---
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Cactos.java
@@ -0,0 +1,14 @@
+package com.example.pdmo.navegador.navegadorweb.gaiagarden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cactos);
+    }
+}
