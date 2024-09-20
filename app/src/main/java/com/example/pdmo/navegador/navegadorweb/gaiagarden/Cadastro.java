4e667816255f3e98103a4384b391d623ebacc201
Luiz <luizotavioms.28@gmail.com>
2024-09-20 10:24:37 -0300

    tcc229

--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Cadastro.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Cadastro.java
@@ -2,7 +2,10 @@ package com.example.pdmo.navegador.navegadorweb.gaiagarden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        Button button = findViewById(R.id.cadastrar);
+        button.setOnClickListener(new View.OnClickListener() {
+        @Override
+                public void onClick(View v){
+            Intent it = new Intent(Cadastro.this, Home.class);
+            startActivity(it);
+        }
+        });
     }
-}
--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Flores.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Flores.java
@@ -2,14 +2,51 @@
 package com.example.pdmo.navegador.navegadorweb.gaiagarden;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Flores extends AppCompatActivity {
 
+    AppCompatActivity floroxa;
+    AppCompatImageView florroxa;
+
+    AppCompatImageView muda;
+
+    AppCompatImageView anturio;
+
+    AppCompatImageView liriobranco;
+
+    AppCompatImageView rosas;
+
+    AppCompatImageView azaleias;
+
+    AppCompatImageView copodeleite;
+
+    AppCompatImageView margaridas;
+
+    AppCompatImageView cravo;
+
+    AppCompatImageView girassol;
+
+    AppCompatImageView liriolaranja;
+
+    AppCompatImageView bromelia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flores);
+
+        floroxa =  findViewById(R.id.floroxa);
+
+        floroxa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Flores.this, DescricaoFlores.class);
+            }
+        });
+
     }
 }
