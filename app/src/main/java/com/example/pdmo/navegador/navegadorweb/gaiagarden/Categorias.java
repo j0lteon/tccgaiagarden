63f49a6f6c544e9938cb66c94ccd3ce834cac90e
rm86270 <rm86270@estudante.fieb.edu.br>
2024-09-20 10:23:49 -0300

    tcc238

--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Categorias.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Categorias.java
@@ -1,17 +1,48 @@
 package com.example.pdmo.navegador.navegadorweb.gaiagarden;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Categorias extends AppCompatActivity {
+
+    AppCompatImageView estrela;
+
+    AppCompatImageView faleconosco;
+
+    AppCompatImageView categoriacacto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
+
+        estrela = findViewById(R.id.estrela);
+
+        estrela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Categorias.this, Meus_Favoritos.class);
+                startActivity(intent);
+            }
+        });
+
+        faleconosco = findViewById(R.id.imgfalecnsc);
+
+        faleconosco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Categorias.this, FaleConosco.class);
+                startActivity(it);
+            }
+        });
+        
 
     }
 }
