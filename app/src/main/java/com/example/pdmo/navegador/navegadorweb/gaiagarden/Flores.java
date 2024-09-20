cf138d5a7d4e982b5bd78408a9106eff4bc6f989
rm86270 <rm86270@estudante.fieb.edu.br>
2024-09-20 11:52:50 -0300

    tcc239

--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Flores.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Flores.java
@@ -10,7 +10,6 @@ import android.view.View;
 
 public class Flores extends AppCompatActivity {
 
-    AppCompatActivity floroxa;
     AppCompatImageView florroxa;
 
     AppCompatImageView muda;
@@ -39,12 +38,13 @@ public class Flores extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flores);
 
-        floroxa =  findViewById(R.id.floroxa);
+        florroxa =  findViewById(R.id.floroxa);
 
-        floroxa.setOnClickListener(new View.OnClickListener() {
+        florroxa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Flores.this, DescricaoFlores.class);
+                startActivity(intent);
             }
         });
 
