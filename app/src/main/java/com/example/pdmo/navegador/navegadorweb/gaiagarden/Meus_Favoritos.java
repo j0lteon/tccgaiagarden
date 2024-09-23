2b2c259bcedb74c86ffa3e81acc4810549d9d6f9
Luiz <luizotavioms.28@gmail.com>
2024-09-23 08:26:57 -0300

    tcc232

--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Meus_Favoritos.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Meus_Favoritos.java
@@ -1,14 +1,32 @@
 package com.example.pdmo.navegador.navegadorweb.gaiagarden;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Meus_Favoritos extends AppCompatActivity {
 
+    AppCompatImageView faleconosco;
+
+    AppCompatImageView home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_meus_favoritos);
+
+        faleconosco = findViewById(R.id.imgfalecnsc);
+
+
+        faleconosco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Meus_Favoritos.this, FaleConosco.class);
+                startActivity(intent);
+            }
+        });
     }
 }
