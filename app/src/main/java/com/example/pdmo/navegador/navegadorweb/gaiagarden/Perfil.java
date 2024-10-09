780e6be06e9ff166ab9737f625ba1f181d36c563
rm86270 <rm86270@estudante.fieb.edu.br>
2024-10-09 17:12:36 -0300

    Criado perfil do usuário, precisa conseguir fazer logout e mostrar o nome do usuário

---
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/gaiagarden/Perfil.java
@@ -0,0 +1,17 @@
+package com.example.pdmo.navegador.navegadorweb.gaiagarden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+
+    }
+}
